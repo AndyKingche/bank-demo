cae92a713e98cbe09691bf2ae6602678336d389c
Andy Quinche <laqm_14@hotmail.com>
2024-08-22 14:56:36 -0500

    feature f2 busqueda de productos

--- src/app/modules/financier/components/product-list/product-list.component.ts
+++ src/app/modules/financier/components/product-list/product-list.component.ts
@@ -12,12 +12,21 @@ import { FormControl } from '@angular/forms';
 export class ProductListComponent implements OnInit {
 
   listProduct : Product[] = [];
+  filterProduct : Product[] = []; // se creó para poder tener los productos filtrados de la busqueda
+  searchControl = new FormControl('');
+
   
   constructor(private financierService: FinancierService) { }
 
   ngOnInit(): void {
 
     this.getlistProduct();
+
+    this.searchControl.valueChanges.subscribe(value =>{
+      this.filterProduct = this.listProduct.filter(product=> 
+        product.name.toLowerCase().includes(value.toLowerCase()));
+    });
+
     
   }
   
@@ -27,13 +36,14 @@ export class ProductListComponent implements OnInit {
       this.financierService.getlistProduct().subscribe((resp:any)=>{
         if(resp){
           this.listProduct = resp.data; 
+          this.filterProduct = resp.data;
           
         }
       });
 
       
     } catch (error) {
-      console.log('ERROR EN GETLISTPRODUCT' , error);
+      console.error('ERROR EN GETLISTPRODUCT' , error);
     }
 
   }
